afe6f925d213d24e78e184b9dc753ec29c711b69
vamsikrish-A <krishnavamsi165@gmail.com>
2021-11-23 00:09:25 +0530

    Added: SimpleServlet class that extends HttpServlet overridie doGet method

---
+++ src/main/java/com/myfirstservlet/SimpleServlet.java
@@ -0,0 +1,20 @@
+package com.myfirstservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/Hello")
+public class SimpleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h3>Hello World Vamsi !!</h3>");
+        out.close();
+    }
+}
